7f6120f34aeb575bade8fc470d09b6f9c0465bcb
RunecGD <gerkarunezz@gmail.com>
2025-09-04 16:49:34 +0300

    Create student list

--- app/src/main/java/com/grsu/runa/buttonNav/students/StudentsFragment.java
+++ app/src/main/java/com/grsu/runa/buttonNav/students/StudentsFragment.java
@@ -1,7 +1,6 @@
 package com.grsu.runa.buttonNav.students;
 
 import android.os.Bundle;
-import android.text.style.TtsSpan;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,9 +8,19 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
-import com.grsu.runa.databinding.FragmentChatsBinding;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.grsu.runa.databinding.FragmentStudentsBinding;
+import com.grsu.runa.users.User;
+import com.grsu.runa.users.UsersAdapter;
+
+import java.util.ArrayList;
 
 public class StudentsFragment extends Fragment {
     FragmentStudentsBinding binding;
@@ -20,7 +29,36 @@ public class StudentsFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         binding= FragmentStudentsBinding.inflate(inflater, container, false);
+        loadUsers();
         return binding.getRoot();
 
     }
+    private void loadUsers(){
+        ArrayList<User> users = new ArrayList<User>();
+        FirebaseDatabase.getInstance("https://runaapp-651b2-default-rtdb.europe-west1.firebasedatabase.app").getReference().child("Users").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for (DataSnapshot userSnapshot : snapshot.getChildren()){
+                    if (userSnapshot.getKey().equals(FirebaseAuth.getInstance().getCurrentUser().getUid())){
+                        continue;
+                    }
+
+                    String username = userSnapshot.child("username").getValue().toString();
+                    String profileImage = userSnapshot.child("profileImage").getValue().toString();
+
+                    users.add(new User(username, profileImage));
+                }
+
+                binding.usersRv.setLayoutManager(new LinearLayoutManager(getContext()));
+                binding.usersRv.addItemDecoration(new DividerItemDecoration(getContext(), DividerItemDecoration.VERTICAL));
+                binding.usersRv.setAdapter(new UsersAdapter(users));
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
 }
+
---
+++ app/src/main/java/com/grsu/runa/users/UsersAdapter.java
@@ -0,0 +1,50 @@
+package com.grsu.runa.users;
+
+import android.text.Layout;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Objects;
+import com.grsu.runa.R;
+
+public class UsersAdapter extends RecyclerView.Adapter<UserViewHolder>{
+
+    private ArrayList<User> users = new ArrayList<>();
+
+    public UsersAdapter(ArrayList<User> users){
+        this.users = users;
+    }
+
+    @NonNull
+    @Override
+    public UserViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.person_item_rv, parent, false);
+        return new UserViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull UserViewHolder holder, int position) {
+        User user = users.get(position);
+
+        holder.username_tv.setText(user.username);
+
+        if (!user.profileImage.isEmpty()){
+            Glide.with(holder.itemView.getContext()).load(user.profileImage).into(holder.profileImage_iv);
+        }
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return users.size();
+    }
+}
