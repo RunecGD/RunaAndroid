f3e71bf51c3788aeb80832b3ad8b0f5251909875
RunecGD <gerkarunezz@gmail.com>
2025-09-07 20:51:54 +0300

    Create func sendMessage and save in db.

--- app/src/main/java/com/grsu/runa/ChatActivity.java
+++ app/src/main/java/com/grsu/runa/ChatActivity.java
@@ -1,12 +1,14 @@
 package com.grsu.runa;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.FirebaseDatabase;
@@ -15,8 +17,12 @@ import com.grsu.runa.databinding.ActivityChatBinding;
 import com.grsu.runa.message.Message;
 import com.grsu.runa.message.MessagesAdapter;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Objects;
 
 
 public class ChatActivity extends AppCompatActivity {
@@ -28,7 +34,19 @@ public class ChatActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
         String chatId = getIntent().getStringExtra("chatId");
         loadMessages(chatId);
+        binding.sendMessageBtn.setOnClickListener(v -> {
+            String message = binding.messageEt.getText().toString();
+            if (message.isEmpty()){
+                Toast.makeText(this, "Message field cannot be empty", Toast.LENGTH_SHORT).show();
+                return;
+            }
 
+            SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd.MM.yyyy HH:mm");
+            String date = simpleDateFormat.format(new Date());
+
+            binding.messageEt.setText("");
+            sendMessage(chatId, message, date);
+        });
     }
     private void loadMessages(String chatId){
 
@@ -58,5 +76,16 @@ public class ChatActivity extends AppCompatActivity {
 
                     }
                 });
+    }
+    private void sendMessage(String chatId, String message, String date){
+        if (chatId==null) return;
+
+        HashMap<String, String> messageInfo = new HashMap<>();
+        messageInfo.put("text", message);
+        messageInfo.put("ownerId", Objects.requireNonNull(FirebaseAuth.getInstance().getCurrentUser()).getUid());
+        messageInfo.put("date", date);
+
+        FirebaseDatabase.getInstance("https://runaapp-651b2-default-rtdb.europe-west1.firebasedatabase.app").getReference().child("Chats").child(chatId)
+                .child("messages").push().setValue(messageInfo);
     }
 }
